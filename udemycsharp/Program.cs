d643702f092e02115db91e225ee794a088911e8e
mert1424 <53661914+mert1424@users.noreply.github.com>
2020-03-31 22:03:22 +0300

    driverlicenseprogram
    Signed-off-by: mert1424 <53661914+mert1424@users.noreply.github.com>

---
+++ udemycsharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace udemycsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("lütfen yasınızı girin:   ");
+            int yas = Convert.ToInt16(Console.ReadLine());
+
+            if (yas>=18)
+            {
+                Console.Write("eğitim durumunuzu girin: ");
+                string egitim = Convert.ToString(Console.ReadLine());
+                if (egitim=="lise" || egitim=="üniversite")
+                {
+                    Console.WriteLine("ehliyet alabilirsiniz.. ");
+                }
+                else
+                {
+                    Console.WriteLine("egitim durumunuz uygun degil.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("18yasında olmadiginiz icin ehliyet alamazsiniz..");
+            }
+        }
+    }
+}
