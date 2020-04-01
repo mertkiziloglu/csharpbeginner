e01ee8129069d59fdcccb9b2323c21ca564fbf6e
mert1424 <53661914+mert1424@users.noreply.github.com>
2020-04-01 17:34:44 +0300

    udemy

---
+++ strctyapisi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace strctyapisi
+{
+    public struct hesaplama
+    {
+        public int kisa;
+        public int uzun;
+
+        public int alan()
+        {
+            return kisa* uzun;
+        }
+
+
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            hesaplama dikdörtgen=new hesaplama();
+
+            dikdörtgen.kisa = 50;
+            dikdörtgen.uzun = 70;
+
+            dikdörtgen.alan();
+            Console.WriteLine(dikdörtgen.alan());
+            
+        }
+    }
+}
---
+++ struct/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace struct
+
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
