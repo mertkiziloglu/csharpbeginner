f2e4f2e9b82838436cde7ff3f77b6221c766268e
mert1424 <53661914+mert1424@users.noreply.github.com>
2020-04-06 02:22:49 +0300

    dizilerörnekalıştırma

---
+++ diziler/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hosgeldiniizzzz !");
+            Console.WriteLine("****************");
+            Console.WriteLine("lütfen 10 tane isim giriniz.");
+            Console.WriteLine("*****************");
+            
+            
+            string[] isim=new string[10];
+            
+            for (int i = 0; i <isim.Length; i++)
+            {
+                Console.Write("isim"+i+"=");
+                isim[i] = Console.ReadLine();
+                
+            }
+
+            Console.WriteLine("******************");
+            Console.WriteLine("aranacak ismi girin lütfen:");
+            Console.WriteLine("*******************");
+            
+            string aranacak = Console.ReadLine();
+            
+                               int sayac = 0;
+            
+            for (int i = 0; i < isim.Length; i++)
+            {
+                if (aranacak==isim[i])
+                {
+                    sayac++;
+                }
+            }
+
+            Console.WriteLine("Aradıginiz kelime "+sayac+" kadardir.");
+            
+        }
+    }
+}
---
+++ dizilerorn2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace dizilerorn2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //kullanıcıdan 20 sayi alan ve tekleri ciftleri toplayan program
+          
+          int [] sayilar=new int[5];
+
+          for (int i = 0; i <5; i++)
+          {
+              Console.WriteLine("lütfen sayi giriniz: ");
+              sayilar[i] = Convert.ToInt16(Console.ReadLine());
+          }
+          Console.Clear();
+           
+          int teksayi = 0, ciftsayi = 0;
+          
+          foreach (int sayi in sayilar)
+          {
+              if (sayi%2==0)
+              {
+                  ciftsayi += sayi;
+              }
+              else
+              {
+                  teksayi += sayi;
+              }
+          }
+          Console.WriteLine("tek sayi: {0}  \ncift sayi:{1}",teksayi,ciftsayi);
+        }
+    }
+}
