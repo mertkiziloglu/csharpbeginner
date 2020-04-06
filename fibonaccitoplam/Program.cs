26d0e3ff1b00304fb3b885404ef2d78787a9909f
mert1424 <53661914+mert1424@users.noreply.github.com>
2020-04-06 16:34:31 +0300

    fibonacci sayılarında çift olanlaarı toplama programı
    Signed-off-by: mert1424 <53661914+mert1424@users.noreply.github.com>

---
+++ fibonaccitoplam/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fibonaccitoplam
+{
+    class Program
+    {
+         public static void Main(string[] args)
+         {
+             int n1 = 0, n2 = 1, n3, i, number, ciftsayi = 0;    
+                 Console.Write("Enter the number of elements: ");    
+                 number = int.Parse(Console.ReadLine());  
+                 for(i=2;i<number;++i)    
+                 {    
+                  n3=n1+n2;    
+                  n1=n2;    
+                  n2=n3;
+                  if (n3%2==0)
+                  {
+                      ciftsayi += n3;
+                  }
+
+                 }
+                 Console.WriteLine("ciftsayitoplami: "+ciftsayi);
+         }  
+    }
+}
