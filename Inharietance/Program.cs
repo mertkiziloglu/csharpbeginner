36cdbc83f33748e042286a9339c1de9815097d3c
mert1424 <53661914+mert1424@users.noreply.github.com>
2020-04-11 23:35:21 +0300

    Inharitance

---
+++ Inharietance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Inharietance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons=new Person[3]
+            {
+                new Customer {FirstName = "mert"}, 
+                new Student {FirstName = "mehmet"}, 
+                new Person {FirstName = "kübra"}
+            };
+            
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
